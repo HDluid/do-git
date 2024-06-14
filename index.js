fe734f2b1ac62869f19302c64b812e70a2f495ec
HDluid <luid.siul@gmail.com>
2024-06-14 22:21:56 +0100

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title> Titulo da pagina</title>
+  </head>
+  <body>
+    <script>
+      let heading = document.createELement('h1');
+
+      head.innerText = 'Texto inicial';
+
+      document.body.appendChild(heading);
+    </script>
+  </body>
+</hmtml>
